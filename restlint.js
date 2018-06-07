94d89f6115b8eab62bec4ff52cc0c2746a6e5ee9
Brett Schwarz <brett.schwarz@gmail.com>
2018-06-07 14:25:46 -0700

    Adds more tests for paths and resources

--- restlint.js
+++ restlint.js
@@ -10,7 +10,7 @@ const fs = require('fs');
 var wordNet = require( 'wordnet-magic' );
 
 var pData = {
-	general: [],
+	general: {},
 	parameters: [],
 	paths: [],
 	status_codes: [],
@@ -27,6 +27,68 @@ var errors = {
 
 var jsdata = '';
 
+
+/**
+* @description gets the properties of 'obj', with a name prefix of 'prefix'
+* @param {string} prefix - the name to prefix to any properties found (for recursion)
+* @param {string} obj - the starting object within the file to start searching
+*/
+function getProps(prefix, obj) {
+	var values = [];
+
+	/**
+	* loop through all the keys extracted from the object
+	*/
+	Object.keys(obj).forEach(function(key, index) {
+		var type = 'object', desc = '';
+		
+		var names = [];
+		if (prefix) {
+			names.push(prefix);
+		}
+		names.push(key);
+
+		if (obj[key].type) {
+			type = obj[key].type;
+		}
+
+		/**
+		* if there is an 'items' object, then look in there for either
+		* a '$ref' to other object, or to get the 'type' of the property
+		* If no 'items' object, then check if there is a '$ref' or 'type'
+		*/
+		if (obj[key].hasOwnProperty('items')) {
+
+			if (obj[key].items.hasOwnProperty('$ref')) {
+					var v = obj[key].items['$ref'];
+					getProps(names.join('.'), jsdata.schemas[v].properties).forEach(function(kk, ii) {
+						names.push(kk);
+						values.push(kk);
+					});
+			} else if (obj[key].items.hasOwnProperty('type')) {
+				values.push(names.join('.') + ',' + obj[key].items.type);
+			}
+		} else if (obj[key].hasOwnProperty('$ref')) {
+			var v = obj[key]['$ref'];
+			getProps(names.join('.'), jsdata.schemas[v].properties).forEach(function(kk, ii) {
+				// names.push(kk);
+
+				names.push(kk);
+				values.push(kk);
+			});
+		} else if (obj[key].hasOwnProperty('type')) {
+				values.push(names.join('.') + ',' + obj[key].type);
+		} else {
+
+			values.push(names.join('.') + ',' + type);
+		}
+	
+	});
+
+	return values;
+}
+
+
 /**
 * @description retrieves the CSV file type based on the name embedded in the file name
 * @param {string} fname - the file name
@@ -39,35 +101,85 @@ function getCsvFileType(fname) {
 	return ftype;
 }
 
+
 /**
 * @description Represents a book
 * @param {string} title - The title of the book
 * @param {string} author - The author of the book
 */
-function checkBasePath(base) {
+function createErrorObj(name, level, msg) {
+	var obj = {};
+	obj.name = name;
+	obj.msg = msg;
+	obj.level = level;
 
+	return obj;
 }
 
 /**
-* @description Represents a book
-* @param {string} title - The title of the book
-* @param {string} author - The author of the book
+* @description checks the base path
 */
-function checkPathsSeparator() {
-	pData.paths.forEach(function(key, idx) {
-		if (key[0] != '/') {
-			var obj = {};
-			obj.name = key;
-			obj.msg = "paths should have leading slash";
-			obj.level = 'warning';
+function checkBasePath() {
+	var x = [];
+	x.push(pData.general.basePath);
+	checkPathStructure('basePath', x);
 
-			errors.paths.push(obj);
-			path = '/' + key;
+	if (! (pData.general.basePath.match(/v[0-9]/g) || []).length) {
+		msg = "basePath should have version string (v[0-9], e.g. v1)";
+		errors.paths.push(createErrorObj(pData.general.basePath, 'error', msg));
+	}
+}
+
+/**
+* @description checks the words that make up a path
+* @param {array} paths - an array of paths
+*/
+function checkResources(paths) {
+	paths.forEach(function(key, idx) {
+		if (/create|make|delete|update|get|del|remove/i.test(key)) {
+			msg = "resource must be a noun";
+			errors.paths.push(createErrorObj(key, 'error', msg));
 		}
 	});
 }
 
-// loop over each of the files that were passed in
+/**
+* @description checks if the paths have the correct path separator
+* @param {string} name - name of the path (paths or basePath)
+* @param {array} paths - array of paths
+*/
+function checkPathStructure(name, paths) {
+	
+	// var allpaths = pData.paths.push(jsdata.basePath);
+
+	paths.forEach(function(key, idx) {
+		var stripped = key.replace(/\//g, '');
+
+		if (/\/\//.test(key)) {
+			msg = name + " can not have double forward slash";
+			errors.paths.push(createErrorObj(key, 'error', msg));
+		}
+		// if (/\?|#|;|=|\s/i.test(key)) {
+		if (stripped != encodeURIComponent(stripped)) {
+			msg = name + " can not have reserved characters";
+			errors.paths.push(createErrorObj(key, 'error', msg));
+		}
+		if (! (key.match(/\//g) || []).length) {
+			msg = name + " should have at least one forward slash";
+			errors.paths.push(createErrorObj(key, 'warning', msg));
+		}
+		if (key[0] != '/') {
+			msg = name + " should have leading forward slash";
+			errors.paths.push(createErrorObj(key, 'error', msg));
+		}
+		if (key[key.length-1] === '/') {
+			msg = name + " should not have forward slash at end of path";
+			errors.paths.push(createErrorObj(key, 'warning', msg));
+		}
+	});
+}
+
+// loop over each of the files that were passed in and loads data
 process.argv.splice(2).forEach(function(item, idx) {
 
 	var filePath = __dirname + '/' + item;
@@ -92,11 +204,16 @@ process.argv.splice(2).forEach(function(item, idx) {
 		
 		var jsdata = JSON.parse(data);
 
+		pData.general.basePath = jsdata.basePath;
+		pData.general.host = jsdata.host;
+
 		Object.keys(jsdata.paths).forEach(function(key, index) {
 			pData.paths.push(key);
+			getProps('', pData.paths[index]).forEach(function(key, idx) {
+				// console.log(key);
+			})
 		});
-
-		console.log(pData.paths.toString());
+		
 	}
 });
 
@@ -105,9 +222,16 @@ process.argv.splice(2).forEach(function(item, idx) {
 // Start running the checks
 //
 
-checkPathsSeparator();
+checkPathStructure('paths', pData.paths);
+checkResources(pData.paths);
+checkBasePath();
 
-console.log(errors.toString());
+
+
+errors.paths.forEach(function(key, idx) {
+
+	console.log(key.level + ': ' + key.name + ' - ' + key.msg);
+});
 
 // var paths = jsdata.paths;
 // var wordnet = new WordNet()
@@ -141,6 +265,6 @@ kiss.getSynsets( function( err, data ) {
 });
 
 kiss.getSynsets()
-.then(
-	console.log
-	);
+.then(function(err,items) {
+		console.log(items);
+});
